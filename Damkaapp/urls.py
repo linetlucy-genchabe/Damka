2bb4466fecff1adae980cb88b8e9d068004215f7
linetlucy-genchabe <linetlucy21@gmail.com>
2024-10-27 20:53:58 +0300

    careers

--- Damkaapp/urls.py
+++ Damkaapp/urls.py
@@ -6,15 +6,17 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.index, name= 'index'),
-     url(r'login/$',views.user_login, name='login'),
+    url(r'login/$',views.user_login, name='login'),
     url(r'logout/$',views.signout),
+    url(r'^vacancies$', views.careers, name= 'vacancies'),
     url(r'^new/activity$', views.new_activity, name='new-activity'),
     url(r'^resources$', views.resources, name= 'resources'),
     url(r'^about$', views.about, name= 'about'),
     url(r'^activities$', views.activities, name= 'activities'),
     url(r'^donate$', views.donate, name= 'donate'),
     url(r'^events$', views.events, name= 'events'),
-     url(r'^careers$', views.careers, name= 'careers'),
+    # url(r'^careers$', views.careers, name= 'careers'),
+
   
   
    
