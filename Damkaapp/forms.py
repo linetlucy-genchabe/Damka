b9a8d531a1068ea6586200ce8b4d2aad3ea4a299
linetlucy-genchabe <linetlucy21@gmail.com>
2024-10-27 16:33:47 +0300

    activity forms

---
+++ Damkaapp/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import *
+from django.contrib.auth.models import User
+
+
+class NewActivityForm(forms.ModelForm):
+    class Meta:
+        model = Activity
+        exclude = ['Author', 'pub_date', 'author_profile','admin_profile', ]
+        widgets = {
+          'activity': forms.Textarea(attrs={'rows':2, 'cols':10,}),
+        }
