99026a50b51514e330f431a75394ff7d1ea20a71
linetlucy-genchabe <linetlucy21@gmail.com>
2024-10-27 16:34:32 +0300

    new activity models

--- Damkaapp/models.py
+++ Damkaapp/models.py
@@ -28,10 +28,49 @@ class Profile(models.Model):
     def __str__(self):
         return str(self.user)
     
-    # def __str__(self):
-    #     return f"{self.user}, {self.bio}, {self.photo}"
-    
     class Meta:
         verbose_name = 'Profile'
         verbose_name_plural = 'Profiles'
             
+class Activity(models.Model):
+    Name = models.CharField(max_length=255)
+    Description = models.CharField(max_length=5000)
+    Image = models.CharField(max_length=255)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    Admin = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    admin_profile = models.ForeignKey(Profile,on_delete=models.CASCADE, blank=True, default='1')
+    
+    
+    def save_activities(self):
+        self.save()
+    
+    def delete_activities(self):
+        self.delete()
+        
+    @classmethod
+    def get_allactivities(cls):
+        accounts = cls.objects.all()
+        return accounts
+    
+   
+    
+    @classmethod
+    def get_activities(request, id):
+        try:
+            account = Activity.objects.get(pk = id)
+            
+        except ObjectDoesNotExist:
+            raise Http404()
+        
+        return account
+    
+    def update_activities(self):
+        self.update_accounts()
+    
+    def __str__(self):
+        return self.Name
+    
+    class Meta:
+        ordering = ['-pub_date']
+        verbose_name = 'My Activities'
+        verbose_name_plural = 'Activities'
