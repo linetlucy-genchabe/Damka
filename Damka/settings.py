0dfbf45c87e38f823511e2ca5507979392597ede
linetlucy-genchabe <linetlucy21@gmail.com>
2024-10-28 09:20:34 +0300

    updated settings

--- Damka/settings.py
+++ Damka/settings.py
@@ -89,7 +89,8 @@ WSGI_APPLICATION = 'Damka.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASE_URL= 'postgresql://postgres:twILokIgLBHFgBmBXxVIqZxetvnpQlGo@monorail.proxy.rlwy.net:13964/railway'
+DATABASE_URL= 'postgresql://postgres:twILokIgLBHFgBmBXxVIqZxetvnpQlGo@postgres.railway.internal:5432/railway'
+
 
 DATABASES = {
     'default': 
